098195988425a750a033b2802f02e89a5cbf2f8f
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-06-26 16:12:57 -0600

    Make App Material
    Added Theme color, this invloves textColors, accent color, etc
    Changed the color of the textview in cardView. one was for primary text, the other was for secondary text
    Changed the toolbar color to color_primary of the theme
    Added Constraint Layout to keep the toolbar and swipeRefresh in order
    Made the swipeRefresh to work properly by adding eventListener to refresh the view by calling the service that makes a asyncTask, returns the books from the internet server, deletes the existing data in the localDB, populates the localDB with the new data fetched.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -67,6 +67,12 @@ public class ArticleListActivity extends AppCompatActivity implements
         final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
